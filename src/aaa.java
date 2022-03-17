3ea270117930d4df1fd3fa06a72080f0070a3a6c
Carlos <c2003velasco@gmail.com>
2022-03-17 19:03:27 +0100

    ou

--- src/aaa.java
+++ src/aaa.java
@@ -4,11 +4,11 @@ public class aaa {
 	public aaa() {
 		// TODO Auto-generated constructor stub
 	}
-
+static int suma = 20;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("bobo");
-
+System.out.println(+suma);
 	}
 
 }
