c29bd610c8d9354fb00a55b2c6a750994e5a0244
Carlos <c2003velasco@gmail.com>
2022-03-17 19:36:51 +0100

    3

--- src/aaa.java
+++ src/aaa.java
@@ -7,7 +7,7 @@ public class aaa {
 static int suma = 20;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("bobo");
+System.out.println("boboaaaaaaaaa");
 System.out.println(+suma);
 	}
 
